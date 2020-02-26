a407c530d965d870d3190429121adeb7de10f04a
Austin Wang <austin.t.wang@gmail.com>
2020-02-26 11:06:18 -0500

    final wall_follower code

--- src/wall_follower.py
+++ src/wall_follower.py
@@ -8,8 +8,7 @@ from sensor_msgs.msg import LaserScan
 from ackermann_msgs.msg import AckermannDriveStamped
 from visualization_msgs.msg import Marker
 from geometry_msgs.msg import Point
-# import tf2_ros
-
+from collections import deque
 
 class PIDController:
     def __init__(self, k_p, k_i, k_d):
@@ -20,8 +19,8 @@ class PIDController:
         self.last_error = None
 
     def __call__(self, error):
-        delta_error = 0 if self.last_error is None else (error - self.last_error)
         self.error_accumulation += error
+        delta_error = 0 if self.last_error is None else (error - self.last_error)
         proportional = self.k_p * error
         integral = self.k_i * self.error_accumulation
         derivative = self.k_d * delta_error
@@ -44,17 +43,15 @@ class WallFollower:
         self.publisher = rospy.Publisher(WallFollower.DRIVE_TOPIC, AckermannDriveStamped, queue_size=10)
         rospy.Subscriber(WallFollower.SCAN_TOPIC, LaserScan, self.callback)
         self.marker_publisher = rospy.Publisher("visualization_marker", Marker, queue_size=10)
-        #         self.tf2_buffer = tf2_ros.Buffer()
-        #         self.tf2_listener = tf2_ros.TransformListener(self.tf2_buffer)
 
         # tunable constants
         distance_k_p = rospy.get_param("wall_follower/distance_k_p", 2.0)
         distance_k_i = rospy.get_param("wall_follower/distance_k_i", 0.0)
-        distance_k_d = rospy.get_param("wall_follower/distance_k_d", 2.0)
+        distance_k_d = rospy.get_param("wall_follower/distance_k_d", 3.0)
+        self.front_dist_threshold_factor = 2
         self.distance_pid = PIDController(distance_k_p, distance_k_i, distance_k_d)
-        self.angle_pid = PIDController(1.0, 0.0, 0.0)
-        self.extra_angle = np.pi / 4
-        self.ransac_residual_threshold = None
+        self.extra_angle = np.pi/4
+        self.ransac_residual_threshold = None 
         self.line_length = 2
 
     def callback(self, laser_scan):
@@ -71,8 +68,7 @@ class WallFollower:
         valid_ranges = (laser_data[0, :] >= scan.range_min) & (laser_data[0, :] <= scan.range_max)
         valid_indices = np.where(valid_ranges)[0]
         laser_data = laser_data[:, valid_indices]
-        valid_angles = ((laser_data[1,
-                         :]) * self.SIDE + self.extra_angle) >= 0  # positive angles if self.SIDE > 0 else negative angles
+        valid_angles = ((laser_data[1, :]) * self.SIDE + self.extra_angle) >= 0  # positive angles if self.SIDE > 0 else negative angles
         side_data = laser_data[:, np.where(valid_angles)[0]]
         front_dist = np.mean(laser_data[0, np.where(np.abs(laser_data[1, :]) < scan.angle_increment * 4)[0]])
         return side_data, front_dist
@@ -115,12 +111,11 @@ class WallFollower:
         return distance, angle
 
     def compute_heading(self, estimated_distance, estimated_wall_angle, front_dist):
-        if front_dist < 2 * self.DESIRED_DISTANCE:
-            return -self.SIDE * self.MAX_HEADING
+        if front_dist < self.front_dist_threshold_factor * self.DESIRED_DISTANCE:
+            return -self.SIDE * self.MAX_HEADING 
         distance_error = self.DESIRED_DISTANCE - estimated_distance
         desired_angle = -self.SIDE * self.distance_pid(distance_error)
-        angle_error = desired_angle - (-1 * estimated_wall_angle)
-        desired_heading = self.angle_pid(angle_error)
+        desired_heading = desired_angle - (-1 * estimated_wall_angle)
         return desired_heading
 
     def generate_marker(self, m, b, marker_id=0):
@@ -133,13 +128,13 @@ class WallFollower:
         marker.action = Marker.ADD
         marker.pose.orientation.w = 0.5
         marker.scale.x = 0.1
-
-        # color is red
+        
+        # color is blue
         marker.color.b = 1.0
         marker.color.a = 1.0
-
-        perp_point = b * np.array([-1.0 / (m + 1 / m), 1.0 / (m ** 2 + 1)])
-        endpoints_x = np.roots([1 + m ** 2,
+        
+        perp_point = b * np.array([-1.0 / (m + 1/m), 1.0 / (m ** 2 + 1)])
+        endpoints_x = np.roots([1 + m ** 2, 
                                 2 * m * (b - perp_point[1]) - 2 * perp_point[0],
                                 perp_point[0] ** 2 + (b - perp_point[1]) ** 2 - (self.line_length / 2) ** 2])
         endpoints_y = m * endpoints_x + b
@@ -150,19 +145,18 @@ class WallFollower:
         end.x = endpoints_x[1]
         end.y = endpoints_y[1]
         marker.points.append(start)
-        marker.points.append(end)
+        marker.points.append(end) 
         self.marker_publisher.publish(marker)
 
     def generate_message(self, desired_heading):
         out_msg = AckermannDriveStamped()
         out_msg.drive.steering_angle = np.clip(desired_heading, a_min=-self.MAX_HEADING, a_max=self.MAX_HEADING)
-        out_msg.drive.steering_angle_velocity = 0  # TODO: tune this
+        out_msg.drive.steering_angle_velocity = 0
         out_msg.drive.speed = self.VELOCITY
-        out_msg.drive.acceleration = 0  # TODO: tune this
-        out_msg.drive.jerk = 0  # TODO: tune this
+        out_msg.drive.acceleration = 0
+        out_msg.drive.jerk = 0
 
         return out_msg
-
 
 if __name__ == "__main__":
     rospy.init_node('wall_follower')
